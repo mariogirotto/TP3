2265ecff961bfb8eef5c20892930aaea22ffe003
santiqwerty <santiago.aguilar@outlook.com>
2014-12-05 18:09:45 -0300

    Create ADT_vector.h

---
+++ ADT_vector.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define INIT_CHOP 20
+
+typedef enum {
+		TRUE,
+		FALSE
+		} bool_t;
+		
+typedef struct {
+		size_t size;
+		void * elements;
+		size_t alloc_size;
+		/*typedef status_t (*destructor_t)(void *) destructor;*/
+		} ADT_vector_t;
+
